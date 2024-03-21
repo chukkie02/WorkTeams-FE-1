6b47a5a4e4f596fa764701def6052010fe99ca52
nel4nelson <an.ekwueme@stu.unizik.edu.ng>
2024-03-21 06:21:09 +0100

    set up landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import LandingPage from "./components/LandingPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
 };
 
 export default App;
